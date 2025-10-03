2e75cb14779187640d864d8b49fb2cd051826d0d
Mahanthi Bukkapatnam <38226830+MahanthiBukkapatnam@users.noreply.github.com>
2025-10-03 14:32:21 -0500

    First Version. Just adding the project shell as a Maven project

---
+++ src/main/java/edu/und/csci465/minipascal/MiniPascalApplication.java
@@ -0,0 +1,9 @@
+package edu.und.csci465.minipascal;
+
+public class MiniPascalApplication {
+
+    public static void main(String[] args) {
+		System.out.println("Hello, this is a Mini Pascal compiler");
+	}
+
+}
